40a0205539e89d039e3b61b9ef6c6fab909cd6a8
Anna Bychkova <bychkova.dev@gmail.com>
2020-10-05 23:33:12 +0300

    Update script-1.js

--- js/script-1.js
+++ js/script-1.js
@@ -1,23 +1,22 @@
 import images from './gallery-items.js';
-// console.log(images);
+console.log(images);
 
-// Разбей задание на несколько подзадач:
+const refs = {
+  galleryContainer: document.querySelector('.js-gallery'),
+  galleryItemEl: document.querySelector('.gallery__item'),
+  modalEl: document.querySelector('div.lightbox'),
+  lightboxOverlayEl: document.querySelector('.lightbox__overlay'),
+  lightboxImageEl: document.querySelector('.lightbox__image'),
+  lightboxCloseButtonEl: document.querySelector(
+    'button[data-action="close-lightbox"]',
+  ),
+};
 
-// Создание и рендер разметки по массиву данных и предоставленному шаблону.
-const galleryContainer = document.querySelector('.js-gallery');
+refs.galleryContainer.addEventListener('click', onContainerClickOpenModal);
 
-const galleryImageEL = document.querySelector('.gallery__image');
+refs.lightboxCloseButtonEl.addEventListener('click', onClickButtonCloseModal);
 
-const modalEl = document.querySelector('div.lightbox');
-const lightboxOverlayEl = document.querySelector('.lightbox__overlay');
-const lightboxImageEl = document.querySelector('.lightbox__image');
-const lightboxCloseButtonEl = document.querySelector(
-  'button[data-action="close-lightbox"]',
-);
-galleryContainer.addEventListener('click', onContainerClickOpenModal);
-
-lightboxCloseButtonEl.addEventListener('click', onCloseModal);
-
+refs.lightboxOverlayEl.addEventListener('click', onOverlayClick);
 // console.log(lightboxButtonEl);
 
 // const option = images[0];
@@ -41,11 +40,9 @@ const listImagesMarkup = images
   })
   .join('');
 
-galleryContainer.insertAdjacentHTML('beforeend', listImagesMarkup);
+refs.galleryContainer.insertAdjacentHTML('beforeend', listImagesMarkup);
 
-console.log(modalEl);
-
-// Реализация делегирования на галерее ul.js - gallery и получение url большого изображения.
+console.log(refs.modalEl);
 
 function onContainerClickOpenModal(evt) {
   console.log(evt);
@@ -54,25 +51,58 @@ function onContainerClickOpenModal(evt) {
     return;
   }
 
-  modalEl.classList.add('is-open');
-  lightboxImageEl.src = evt.target.dataset.source;
-  lightboxImageEl.alt = evt.target.alt;
+  refs.modalEl.classList.add('is-open');
+  refs.lightboxImageEl.src = evt.target.dataset.source;
+  refs.lightboxImageEl.alt = evt.target.alt;
+  window.addEventListener('keydown', onEscKeyPress);
+  window.addEventListener('keydown', onRightKeyPress);
 }
 
-function onCloseModal(evt) {
+function onRightKeyPress(evt) {
+  if (evt.code === 'ArrowRight') {
+  }
+}
+
+function onClickButtonCloseModal(evt) {
   evt.preventDefault();
   if (!evt.target.classList.contains('lightbox__button')) {
     return;
   }
-  modalEl.classList.remove('is-open');
-  lightboxImageEl.src = '';
-  lightboxImageEl.alt = '';
+  onCloseModal();
 }
 
+function onOverlayClick(evt) {
+  if (!evt.target.classList.contains('lightbox__overlay')) {
+    return;
+  }
+  onCloseModal();
+}
+
+function onEscKeyPress(evt) {
+  if (evt.code === 'Escape') {
+    onCloseModal();
+  }
+}
+
+function onCloseModal() {
+  refs.modalEl.classList.remove('is-open');
+  refs.lightboxImageEl.src = '';
+  refs.lightboxImageEl.alt = '';
+}
+
+// Разбей задание на несколько подзадач:
+
+// Создание и рендер разметки по массиву данных и предоставленному шаблону.
 // Открытие модального окна по клику на элементе галереи.
+// Реализация делегирования на галерее ul.js - gallery и получение url большого изображения.
 // Подмена значения атрибута src элемента img.lightbox__image.
 
 // Закрытие модального окна по клику на кнопку button[data - action= "close-lightbox"].
 // Очистка значения атрибута src элемента img.lightbox__image.
 // Это необходимо для того, чтобы при следующем открытии модального окна,
-// пока грузится изображение, мы не видели предыдущее.
+// // пока грузится изображение, мы не видели предыдущее.
+// Следующий функционал не обязателен при сдаче задания, но будет хорошей практикой по работе с событиями.
+
+// Закрытие модального окна по клику на div.lightbox__overlay.
+// Закрытие модального окна по нажатию клавиши ESC.
+// Пролистывание изображений галереи в открытом модальном окне клавишами "влево" и "вправо".
